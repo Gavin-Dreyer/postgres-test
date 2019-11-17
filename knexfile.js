0a3f985c390b758424317c7bbcf9bee76181149e
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-17 13:24:42 -0800

    test

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,18 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
+    },
+    migration: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   },
 
@@ -13,7 +20,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -26,18 +33,15 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
     }
   }
 
