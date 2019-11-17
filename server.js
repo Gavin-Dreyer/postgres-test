8d206c4170b27bdec512047700c0dc9b5d98de62
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-17 14:04:24 -0800

    /users

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
+const db = require('../data/db-config.js');
+
 const server = express()
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({ hello: 'world' })
+})
+
+server.get('/users', (req, res) => {
+    db(users).then(response => {
+        res.json(response)
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to get resources' });
+    });
 })
 
 
